46334633936f8315cf09a72c8f967f052e4ad46f
Wutipong Wongsakuldej <wutipong@trueaxiongames.com>
2019-11-23 20:23:38 +0700

    fix the thumbnail size (again)

--- browse.go
+++ browse.go
@@ -44,7 +44,7 @@ func createItems(files []string) []item {
 		var thumbURL string
 
 		url = "/view/" + f
-		thumbURL = "/get_image/" + f + "?i=0"
+		thumbURL = "/get_image/" + f + "?i=0;width=200"
 
 		output[i] = item{
 			Name:     f,
