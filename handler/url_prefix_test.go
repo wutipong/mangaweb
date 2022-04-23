862427332203f90ecd6b2ad129a1aa0eac29b4f4
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-04-23 23:18:57 +0700

    add tests for handler's url functions.

---
+++ handler/url_prefix_test.go
@@ -0,0 +1,97 @@
+package handler
+
+import (
+	"github.com/stretchr/testify/suite"
+	"github.com/wutipong/mangaweb/util"
+	"path/filepath"
+	"testing"
+)
+
+type URLPrefixTestSuite struct {
+	suite.Suite
+	FilePath string
+}
+
+func (suite *URLPrefixTestSuite) SetupSuite() {
+	util.SetPrefix("/manga")
+	Init(Options{
+		PathRoot:          "/",
+		PathBrowse:        "/browse",
+		PathView:          "/view",
+		PathStatic:        "/static",
+		PathGetImage:      "/get_image",
+		PathUpdateCover:   "/update_cover",
+		PathThumbnail:     "/thumbnail",
+		PathFavorite:      "/favorite",
+		PathDownload:      "/download",
+		PathRescanLibrary: "/rescan_library",
+	})
+
+	suite.FilePath = filepath.Join("同人マンガ", "エロいまんが.zip")
+
+	util.SetPrefix("/manga")
+}
+
+func (suite *URLPrefixTestSuite) TestCreateViewURL() {
+	u := CreateViewURL(suite.FilePath)
+
+	suite.Assert().Equal(
+		"/manga/view/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateRescanURL() {
+	u := CreateRescanURL()
+	suite.Assert().Equal("/manga/rescan_library", u)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateGetImageURL() {
+	u := CreateGetImageURL(suite.FilePath, 50)
+	suite.Assert().Equal(
+		"/manga/get_image/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip?i=50",
+		u)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateUpdateCoverURL() {
+	u := CreateUpdateCoverURL(suite.FilePath, 50)
+	suite.Assert().Equal(
+		"/manga/update_cover/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip?i=50",
+		u,
+	)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateDownloadURL() {
+	u := CreateDownloadURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/manga/download/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateSetFavoriteURL() {
+	u := CreateSetFavoriteURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/manga/favorite/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateThumbnailURL() {
+	u := CreateThumbnailURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/manga/thumbnail/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLPrefixTestSuite) TestCreateBrowseURL() {
+	u := CreateBrowseURL("aabbcc")
+	suite.Assert().Equal(
+		"/manga/browse#aabbcc",
+		u,
+	)
+}
+
+func TestURLPrefixTestSuite(t *testing.T) {
+	suite.Run(t, new(URLPrefixTestSuite))
+}
---
+++ handler/url_test.go
@@ -0,0 +1,95 @@
+package handler
+
+import (
+	"github.com/stretchr/testify/suite"
+	"github.com/wutipong/mangaweb/util"
+	"path/filepath"
+	"testing"
+)
+
+type URLTestSuite struct {
+	suite.Suite
+	FilePath string
+}
+
+func (suite *URLTestSuite) SetupSuite() {
+	Init(Options{
+		PathRoot:          "/",
+		PathBrowse:        "/browse",
+		PathView:          "/view",
+		PathStatic:        "/static",
+		PathGetImage:      "/get_image",
+		PathUpdateCover:   "/update_cover",
+		PathThumbnail:     "/thumbnail",
+		PathFavorite:      "/favorite",
+		PathDownload:      "/download",
+		PathRescanLibrary: "/rescan_library",
+	})
+
+	suite.FilePath = filepath.Join("同人マンガ", "エロいまんが.zip")
+	util.SetPrefix("")
+}
+
+func (suite *URLTestSuite) TestCreateViewURL() {
+	u := CreateViewURL(suite.FilePath)
+
+	suite.Assert().Equal(
+		"/view/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u)
+}
+
+func (suite *URLTestSuite) TestCreateRescanURL() {
+	u := CreateRescanURL()
+	suite.Assert().Equal("/rescan_library", u)
+}
+
+func (suite *URLTestSuite) TestCreateGetImageURL() {
+	u := CreateGetImageURL(suite.FilePath, 50)
+	suite.Assert().Equal(
+		"/get_image/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip?i=50",
+		u)
+}
+
+func (suite *URLTestSuite) TestCreateUpdateCoverURL() {
+	u := CreateUpdateCoverURL(suite.FilePath, 50)
+	suite.Assert().Equal(
+		"/update_cover/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip?i=50",
+		u,
+	)
+}
+
+func (suite *URLTestSuite) TestCreateDownloadURL() {
+	u := CreateDownloadURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/download/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLTestSuite) TestCreateSetFavoriteURL() {
+	u := CreateSetFavoriteURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/favorite/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLTestSuite) TestCreateThumbnailURL() {
+	u := CreateThumbnailURL(suite.FilePath)
+	suite.Assert().Equal(
+		"/thumbnail/%E5%90%8C%E4%BA%BA%E3%83%9E%E3%83%B3%E3%82%AC/%E3%82%A8%E3%83%AD%E3%81%84%E3%81%BE%E3%82%93%E3%81%8C.zip",
+		u,
+	)
+}
+
+func (suite *URLTestSuite) TestCreateBrowseURL() {
+	u := CreateBrowseURL("aabbcc")
+	suite.Assert().Equal(
+		"/browse#aabbcc",
+		u,
+	)
+}
+
+func TestURLTestSuite(t *testing.T) {
+	suite.Run(t, new(URLTestSuite))
+}
