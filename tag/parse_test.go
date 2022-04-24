42801c07a419e0dd130467c85e764330f4003b82
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-04-24 19:28:20 +0700

    add Tag parsing

---
+++ tag/parse_test.go
@@ -0,0 +1,19 @@
+package tag
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseTagOne(t *testing.T) {
+	assert.ElementsMatch(t, ParseTag("[Test]Some weird name"), []string{"Test"})
+}
+
+func TestParseTagNoTag(t *testing.T) {
+	assert.Empty(t, ParseTag("Hello World"))
+}
+
+func TestParseTagMultiple(t *testing.T) {
+	assert.ElementsMatch(t, ParseTag("[Test]Some weird name [Download]"), []string{"Test", "Download"})
+}
