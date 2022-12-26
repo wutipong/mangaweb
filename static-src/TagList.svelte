c2dfa4029d4fe18c510b6d937d29ac93a15aaa5d
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-12-27 01:20:30 +0700

    Implement TagList Item.svelte

--- static-src/TagList.svelte
+++ static-src/TagList.svelte
@@ -1,10 +1,31 @@
 <script>
     import Toolbar from "./TagList/Toolbar.svelte";
+    import Item from "./TagList/Item.svelte";
 
     export let params
     export let browseURL = ""
     export let tagListURL = ""
 
+    let favoriteOnly = false
+
+    function toggleFavoriteFilter() {
+        favoriteOnly = !favoriteOnly
+        if (favoriteOnly) {
+            $('.not-favorite').addClass('d-none')
+            $('#filter-favorite').addClass('active')
+        } else {
+            $('.not-favorite').removeClass('d-none')
+            $('#filter-favorite').removeClass('active')
+        }
+    }
 </script>
 
 <Toolbar title="{params.Title}" browseURL="{browseURL}" tagListURL="{tagListURL}" onToggleFavoriteFilter=""></Toolbar>
+
+<div class='container-fluid' style='padding-top:100px;'>
+    <div class='grid-container'>
+        {#each params.Tags as tag}
+            <Item name={tag.Name} favorite={tag.Favorite} id={tag.ID} url={tag.URL} thumbnailURL={tag.ThumbnailURL}></Item>
+        {/each}
+    </div>
+</div>
---
+++ static-src/TagList/Item.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    export let name = ""
+    export let favorite = false
+    export let id = ""
+    export let url = ""
+    export let thumbnailURL = ""
+</script>
+
+<div class='card mb-3'
+     class:border={favorite}
+     class:border-pink={favorite}
+     class:favorite={favorite}
+     class:not-favorite={!favorite}
+     id='{id}'>
+
+    {#if favorite}
+    <span class='position-absolute bottom-0 end-0 p-2 badge bg-pink'>
+        <span><i class='bi bi-star-fill'></i> Favorite</span>
+    </span>
+    {/if}
+
+    <div class='row g-0'>
+        <div class='col-md-4'>
+            <a href='{url}' class='thumb-link'>
+                <img class='img-fluid lazy rounded thumb-img' loading='lazy' src='{thumbnailURL}'
+                     alt='{name}'>
+            </a>
+        </div>
+        <div class='col-md-8'>
+            <div class='card-body'>
+                <h5 class='card-title'><a href='{url}'>{name}</a></h5>
+            </div>
+        </div>
+    </div>
+</div>
