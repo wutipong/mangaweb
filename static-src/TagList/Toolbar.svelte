6a05b3ea30c02bb84085ba1455b7b87f71451594
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-12-26 21:46:12 +0700

    Add Toolbar.svelte

---
+++ static-src/TagList/Toolbar.svelte
@@ -0,0 +1,59 @@
+<script>
+    export let title = ""
+    export let browseURL = ""
+    export let tagListURL = ""
+    export let onToggleFavoriteFilter
+
+    function toggleFavoriteFilter() {
+        (onToggleFavoriteFilter !== undefined)? onToggleFavoriteFilter():null
+    }
+</script>
+
+<nav class='navbar navbar-dark bg-dark fixed-top navbar-expand-lg'>
+    <div class='container-fluid'>
+        <span class='navbar-brand text-truncate'>{title}</span>
+
+        <button class='navbar-toggler' type='button' data-bs-toggle='collapse'
+                data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent'
+                aria-expanded='false' aria-label='Toggle navigation'>
+
+            <span class='navbar-toggler-icon'></span>
+        </button>
+
+        <div class='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul class='navbar-nav me-auto mb-2 mb-lg-0'>
+                <li class='nav-item dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbarBrowseDropdown'
+                       role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+                        Browse
+                    </a>
+                    <ul class='dropdown-menu' aria-labelledby='navbarBrowseDropdown'>
+                        <li>
+                            <a class='dropdown-item' href='{browseURL}'>
+                                <i class='bi bi-list-ul'></i> All items
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class='dropdown-item' href='{tagListURL}'>
+                                <i class="bi bi-tags-fill"></i> Tag list
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class='nav-item dropdown'>
+                    <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button'
+                       data-bs-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                        Filter
+                    </a>
+                    <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                        <button class='dropdown-item' type='button'
+                                id='filter-favorite' on:click={toggleFavoriteFilter}>
+                            <i class='bi bi-star-fill'></i> Favorite
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
--- static-src/tag_list.ts
+++ static-src/tag_list.ts
@@ -1,11 +1,15 @@
 import TagList from './TagList.svelte'
 
 const params = document.currentScript.getAttribute('data-params')
+const browseURL = document.currentScript.getAttribute('data-browse-url')
+const tagListURL = document.currentScript.getAttribute('data-tag-list-url')
 
 const app = new TagList({
     target: document.getElementById('app'),
     props: {
-        params: JSON.parse(params)
+        params: JSON.parse(params),
+        browseURL: browseURL,
+        tagListURL: tagListURL,
     },
 })
 
