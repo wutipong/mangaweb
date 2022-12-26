20e4c2d3d6a20238aa1a880b499ce554bca0840a
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-12-26 20:39:44 +0700

    parse parameter to browse app via data-param

--- static-src/browse.ts
+++ static-src/browse.ts
@@ -1,4 +1,12 @@
-import type Browse from './Browse.svelte'
+import Browse from './Browse.svelte'
 
+const params = document.currentScript.getAttribute('data-params')
 
-export { type Browse }
+const app = new Browse({
+    target: document.getElementById('app'),
+    props: {
+        params: JSON.parse(params)
+    },
+})
+
+export default app;
