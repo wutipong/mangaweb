d3a117feead57c02b7ba95c9c1b0579b259ec689
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-12-28 21:13:47 +0700

    Imeplement Pagination.

---
+++ static-src/Common/Pagination.svelte
@@ -0,0 +1,5 @@
+<nav aria-label='Page navigation' class='position-fixed bottom-0 start-50 p-3 translate-middle-x'>
+    <ul class='pagination justify-content-center'>
+        <slot/>
+    </ul>
+</nav>
