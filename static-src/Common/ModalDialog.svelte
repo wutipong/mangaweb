66a1a449b89a61f149198ac3bee0c4fe7a4fca85
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2022-12-28 20:32:04 +0700

    Add ModalDialog.svelte

---
+++ static-src/Common/ModalDialog.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {Modal} from 'bootstrap'
+
+    export let Tittle=""
+    export let Id =""
+
+    function show(){
+        const dialog = new Modal(`#${Id}`);
+        dialog.show()
+    }
+</script>
+
+<div class='modal fade' id={Id} tabindex='-1'>
+    <div class='modal-dialog'>
+        <div class='modal-content'>
+            <div class='modal-header'>
+                <h5 class='modal-title'>{Tittle}</h5>
+                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+            </div>
+            <div class='modal-body'>
+                <slot></slot>
+            </div>
+            <div class='modal-footer'>
+                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+            </div>
+        </div>
+    </div>
+</div>
