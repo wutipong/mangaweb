fc51a69251da67236459c6d6182c883362b61fa6
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-08-24 12:53:06 +0700

    update Toast

--- static-src/Common/Toast.svelte
+++ static-src/Common/Toast.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-    import { Toast } from "bootstrap";
+    import { Toast, ToastHeader, ToastBody } from "sveltestrap/src";
 
     let message = "";
     let header = "";
 
+    let isOpen = false;
     export function show(h: string, m: string) {
         message = m;
         header = h;
 
-        const toast = new Toast(document.getElementById("toast"));
-        toast.show();
+        isOpen = true;
+    }
+
+    function toggle() {
+        isOpen = !isOpen;
     }
 </script>
 
@@ -17,24 +21,8 @@
     class="toast-container position-fixed bottom-0 end-0 p-3"
     style="z-index: 11"
 >
-    <div
-        id="toast"
-        class="toast"
-        role="alert"
-        aria-live="assertive"
-        aria-atomic="true"
-    >
-        <div class="toast-header">
-            <strong class="me-auto" id="toast-header">{header}</strong>
-            <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="toast"
-                aria-label="Close"
-            />
-        </div>
-        <div class="toast-body" id="toast-content">
-            {message}
-        </div>
-    </div>
+    <Toast autohide {isOpen} on:close={() => (isOpen = false)}>
+        <ToastHeader {toggle}>{header}</ToastHeader>
+        <ToastBody>{message}</ToastBody>
+    </Toast>
 </div>
